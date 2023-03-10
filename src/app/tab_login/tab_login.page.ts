626ea2d3d5cfa014ebd9b012398aa233adfd6f7d
LAMIRAULT.E Esteban LAMIRAULT <LAMIRAULT.E@epid-vauban.fr>
2023-03-10 16:43:38 +0100

    Commit

--- src/app/tab_login/tab_login.page.ts
+++ src/app/tab_login/tab_login.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-tab_login',
@@ -9,9 +10,32 @@ export class TabLoginPage {
 
   Name:string;
   Password:string;
-
   test:string = "Password";
+  ionicForm:FormGroup;
+  isSubmitted = false;
 
-  constructor() {}
+  constructor(public formBuilder:FormBuilder) {}
 
+  ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      user: ['', [Validators.required, Validators.minLength(2)]],
+      pass: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    })
+  }
+
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  //Déclenche le formuaire lorsque "Connexion" appuyé
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Veuillez remplire tous les champs.')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+      return true;
+    }
+  }
 }

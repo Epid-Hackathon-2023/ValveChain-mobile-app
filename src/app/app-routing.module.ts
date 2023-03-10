d5e116fb2a434a6d9d30fabf19900e417e661e09
Reshiban <reshibangaming10@gmail.com>
2023-03-10 22:43:22 +0100

    Improve login advancement + miscellaneous
    [login advancements]
    -Added form tag
    -Added some conditions to name/pass
    -Renamed ionicForm to loginForm
    
    [miscellaneous]
    -Added some stuff to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)}
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
+  { path: 'tab_login', loadChildren: () => import('./tab_login/tab_login.module').then(m => m.TabLoginPageModule)},
+  { path: 'tab_home', loadChildren: () => import('./tab_home/tab_home.module').then(m => m.TabHomePageModule)},
+  { path: 'tab_fill', loadChildren: () => import('./tab_fill/tab_fill.module').then(m => m.TabFillPageModule)},
+  { path: 'tab_settings', loadChildren: () => import('./tab_settings/tab_settings.module').then(m => m.TabSettingsPageModule)}
+  
 /*{ path: '', redirectTo: 'home', pathMatch: 'full' },
  *{ path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
  *{ path: 'valvedb/:id', loadChildren: () => import('./valvedb/valvedb.module').then( m => m.ValvedbPageModule)},
--- src/app/tab_login/tab_login.page.ts
+++ src/app/tab_login/tab_login.page.ts
@@ -11,30 +11,30 @@ export class TabLoginPage {
   Name:string;
   Password:string;
   test:string = "Password";
-  ionicForm:FormGroup;
+  loginForm:FormGroup;
   isSubmitted = false;
 
   constructor(public formBuilder:FormBuilder) {}
 
   ngOnInit() {
-    this.ionicForm = this.formBuilder.group({
+    this.loginForm = this.formBuilder.group({
       user: ['', [Validators.required, Validators.minLength(2)]],
-      pass: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      pass: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]')]]
     })
   }
 
   get errorControl() {
-    return this.ionicForm.controls;
+    return this.loginForm.controls;
   }
 
   //Déclenche le formuaire lorsque "Connexion" appuyé
   submitForm() {
     this.isSubmitted = true;
-    if (!this.ionicForm.valid) {
+    if (!this.loginForm.valid) {
       console.log('Veuillez remplire tous les champs.')
       return false;
     } else {
-      console.log(this.ionicForm.value)
+      console.log(this.loginForm.value)
       return true;
     }
   }
